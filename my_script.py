54dadcb02dc54fa9dba089384f35043f45d0c6f7
ramchander7 <helloram07@gmail.com>
2018-10-01 09:59:13 -0500

    second commit

--- my_script.py
+++ my_script.py
@@ -3,3 +3,4 @@ from pystrich.datamatrix import DataMatrixEncoder
 encoder = DataMatrixEncoder('This is a DataMatrix.')
 encoder.save('./datamatrix_test.png')
 print(encoder.get_ascii())
+#this is my second commit
